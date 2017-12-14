9502285563ca97b70cfbaacf3f3f7033b80e6cc7
zachary0447 <82210447@qq.com>
2017-12-15 01:13:59 +0800

    给简历加JS

--- main.js
+++ main.js
@@ -1,1 +1,48 @@
-siteWelcome.classList.remove('active');
+setTimeout(function () {
+    siteWelcome.classList.remove('active');
+}, 500);
+
+window.onscroll = function (xxx) {
+    if (window.scrollY > 0) {
+        topNavBar.classList.add('sticky')
+    }
+    else {
+        topNavBar.classList.remove('sticky')
+    }
+}
+
+let aTags = document.getElementsByClassName('menuTrigger')
+{
+    for(let i=0; i<aTags.length; i++){
+        aTags[i].onmouseenter = function(x){
+            let li = x.currentTarget
+            let n = 0
+            while (li.children[n].tagName !== 'UL'){
+                n = n + 1;
+            }
+            li.children[n].classList.add('active');
+        }
+        aTags[i].onmouseleave = function(x){
+            let li = x.currentTarget
+            let n = 0
+            while (li.children[n].tagName !== 'UL'){
+                n = n + 1;
+            }
+            li.children[n].classList.remove('active');
+        }
+        
+    }
+}
+
+let scrollTags = document.querySelectorAll('nav.menu > ul > li')
+{
+    for(let i=0; i<scrollTags.length; i++){
+        scrollTags[i].onclick = function(x){
+            let a = x.currentTarget;
+            let href = a.getAttribute('href');
+            let element = document.querySelector(href);
+            let top = element.offsetTop;
+            window.scrollTo(0, top - 70);
+        }
+    }
+}
